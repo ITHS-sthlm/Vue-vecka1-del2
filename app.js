b8d61029713a17b18aea19ac3400fc2b04038a60
Navigoo <81353827+Navigoo@users.noreply.github.com>
2024-01-10 20:51:44 +0100

    first

---
+++ app.js
@@ -0,0 +1,19 @@
+/* Welcome to week 3 */
+
+const message = Vue.createApp({
+  data(){
+    return {
+      message: "Vue vecka 1 del 2"
+    }
+  }
+});
+message.mount('#message');
+
+/* Part 1, events handler */
+
+/* Part 1 continues with a shopping list */
+
+/* Part 2 - Computed vs not Computed */
+
+/* Part 3 -Watch- check if there is any changes in a certain variable */
+
