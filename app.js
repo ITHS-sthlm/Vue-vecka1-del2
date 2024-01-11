6653dc8e6876ee58a6d39844b71e231ccb7858e9
Navigoo <81353827+Navigoo@users.noreply.github.com>
2024-01-11 13:31:27 +0100

    Klar

--- app.js
+++ app.js
@@ -1,19 +1,84 @@
-/* Welcome to week 3 */
+/* Del2 */
 
 const message = Vue.createApp({
-  data(){
-    return {
-      message: "Vue vecka 1 del 2"
+    data() {
+        return {
+            message: 'Vue vecka 1 del 2'
+        };
     }
-  }
 });
 message.mount('#message');
 
 /* Part 1, events handler */
-
-/* Part 1 continues with a shopping list */
+const handler = Vue.createApp({
+    data() {
+        return {
+            counter: 2,
+            name: ''
+        };
+    },
+    methods: {
+        add() {
+            this.counter++;
+        },
+        reduce() {
+            this.counter--;
+        },
+        setName(event) {
+            this.name = event.target.value;
+        }
+    }
+});
+handler.mount('#handler');
 
 /* Part 2 - Computed vs not Computed */
+const computed = Vue.createApp({
+    data() {
+        return {
+            counter: 2,
+            firstName: ''
+        };
+    },
+    methods: {
+        add() {
+            this.counter++;
+        },
+        reduce() {
+            this.counter--;
+        }
+    },
+    computed: {
+        giveFullName() {
+            console.log('funktionen är igång');
+        }
+    }
+});
+computed.mount('#computed');
 
 /* Part 3 -Watch- check if there is any changes in a certain variable */
-
+const watcher = Vue.createApp({
+    data() {
+        return {
+            firstName: '',
+            lastName: '',
+            fullName: ''
+        };
+    },
+    watch: {
+        firstName(value) {
+            if (value === 'Richard') {
+                this.fullName = value + ' ' + 'Carlssson';
+            } else {
+                this.fullName = value;
+            }
+        },
+        lastName(value) {
+            if (value !== '') {
+                this.fullName = this.firstName + ' ' + value;
+            } else if (value === '') {
+                this.fullName = this.firstName + ' ' + 'Carlsson';
+            }
+        }
+    }
+});
+watcher.mount('#watcher');
